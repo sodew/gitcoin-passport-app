f15242a745feb97c418bf26392cbec322be69adc
sophiahdew <59624772+sophiahdew@users.noreply.github.com>
2023-05-21 23:09:41 -0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Libre_Franklin } from 'next/font/google'
 
+const libreFranklin = Libre_Franklin({ subsets: ['latin'] })
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={libreFranklin.className}>{children}</body>
     </html>
   )
 }
